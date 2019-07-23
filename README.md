409a2ede01c3dfc659fc5ea99335995caff7fd59
violet chang <violet7chang@gmail.com>
2019-07-22 22:45:33 -0500

    updated readme, server.js, some html

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # FriendFinder
+
+A full-stack site that takes in results from a user survey, then compares those results with the results of other users. The app will then display the name and photo of the user with the best overall match.
+
+## Built With
+
+- HTML5
+- javaScript
+- node.JS
+- JSON
+- Express
+- Heroku
+
+### Violet Chang - Developer
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+
+//tells node we're creating an "express server"
+var app = express();
+
+//sets initial port
+var PORT = process.env.PORT || 8080;
+
+//sets up the express app to handle data parsing
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//ROUTER
+//points our server to a series of "route files"
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//LISTENER
+//the below code "starts" our server
+app.listen(PORT, function(){
+    console.log("Listening on PORT: ", PORT)
+});
