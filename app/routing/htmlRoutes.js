149bc00e1c422f7c7a017c2c6d7c67f122927547
violet chang <violet7chang@gmail.com>
2019-07-25 23:11:42 -0500

    htmlroutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,12 +15,8 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
-  });
-
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
